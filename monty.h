08373ce8c7385293ab52e60b20a628ab76a67a75
Tihitna-22 <tihitna222sisay@gmail.com>
2021-12-03 05:53:48 -0800

    modify monty.h

--- monty.h
+++ monty.h
@@ -1,5 +1,7 @@
 #ifndef __MONTYH__
 #define __MONTYH__
+#define  _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -32,14 +34,17 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-void get_func(char *command, stack_t **stack, unsigned int linenum);
-void push(stack_t **stack, unsigned int linenum);
-void pall(stack_t **stack, unsigned int linenum);
+void get_func(char *command, stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
 void freestack(stack_t *stack);
-void pint(stack_t **stack, unsigned int linenum);
-void pop(stack_t **stack, unsigned int linenum);
-void swap(stack_t **stack, unsigned int linenum);
-void add(stack_t **stack, unsigned int linenum);
-void nop(stack_t **stack, unsigned int linenum);
-void sub(stack_t **stack, unsigned int linenum);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void div_op(stack_t **stack, unsigned int line_number);
+void mul_op(stack_t **stack, unsigned int line_number);
+
 #endif
