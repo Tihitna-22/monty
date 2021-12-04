0fabadd54e19d514f345a913ccc69a87fefdddfa
Tihitna-22 <tihitna222sisay@gmail.com>
2021-12-03 22:22:51 -0800

    modify sub.c on error

--- sub.c
+++ sub.c
@@ -12,9 +12,8 @@ void sub(stack_t **stack, unsigned int line_number)
 
         if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
         {
-                fprintf(stderr, "L%d: can't sub, stack too short", line_number)\
-;
-                exit(EXIT_FAILURE);
+	  fprintf(stderr, "L%d: can't sub, stack too short", line_number);
+	  exit(EXIT_FAILURE);
         }
         data = 0;
         data += (*stack)->n;
