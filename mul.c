cda13be29ce6316cd81dd6584b6be9a110572694
Tihitna-22 <tihitna222sisay@gmail.com>
2021-12-03 22:40:15 -0800

    modify mul.c

--- mul.c
+++ mul.c
@@ -12,7 +12,7 @@ void mul_op(stack_t **stack, unsigned int line_number)
 
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't mul, stack too short \n", line_number);
+	        fprintf(stderr, "L%d: can't mul, stack too short \n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
