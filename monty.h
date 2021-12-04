499eef05d51b0e9147b3d6f5ea2aa3bd6c73303a
Tihitna-22 <tihitna222sisay@gmail.com>
2021-12-03 22:54:49 -0800

    modify for mod.c

--- monty.h
+++ monty.h
@@ -46,5 +46,5 @@ void nop(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
 void div_op(stack_t **stack, unsigned int line_number);
 void mul_op(stack_t **stack, unsigned int line_number);
-
+void mod(stack_t **stack, unsigned int line_number);
 #endif
